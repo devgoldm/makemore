83a680ef9d0a46a0585797c36b77e452f5ac4020
goldm.dev <m.goldman90@gmail.com>
2024-07-12 14:32:08 +0100

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y netcat-openbsd
+# use --browser if you want to use Jupyter notebook in your browser instead of vscode
+for arg in "$@"; do
+    if [ "$arg" == "--browser" ]; then
+        sudo apt-get update
+        sudo apt-get install -y netcat-openbsd
+    fi
+done
+
 pip install -r requirements.txt
